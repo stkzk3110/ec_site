1ac8ccfbd08ebef0d7640057c0c33f65b360ced5
fisherman0825 <fisherman0825dd@gmail.com>
2019-03-17 22:53:48 +0900

    fix third commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,2 +1,9 @@
 class ImageUploader < ApplicationUploader
+    def extension_whitelist
+        %w(jpg jpeg gif png)
+    end
+    def accept_extensions
+        accept_list = extension_whitelist.map { |e| "image/#{e}" }
+        accept_list.join(",")
+    end
 end

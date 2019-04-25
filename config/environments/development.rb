cc34772ff903356ecd035051733f2e4b5feece6c
fisherman0825 <fisherman0825dd@gmail.com>
2019-04-26 01:03:02 +0900

    add heroku support2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
+  config.web_console.development_only = false
 end
